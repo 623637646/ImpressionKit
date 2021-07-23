bedb28946e324a7be60c564993660b0a69f33f21
PangMo5 <a3457709@icloud.com>
2021-07-23 11:22:32 +0900

    update SwiftUI Demo

--- ImpressionKitExample/ImpressionKitExample/SwiftUIDemoView.swift
+++ ImpressionKitExample/ImpressionKitExample/SwiftUIDemoView.swift
@@ -10,19 +10,30 @@ import ImpressionKit
 import SwiftUI
 
 @available(iOS 13.0, *)
-struct SwiftUIDemoView: View {
-    var group = ImpressionGroup.init { (_, index: Int, _, state) in
+final class SwiftUIDemoViewModel: ObservableObject {
+    lazy var group = ImpressionGroup.init { [weak self] (_, index: Int, _, state) in
         if state.isImpressed {
             print("impressed index: \(index)")
         }
+        self?.list[index].1 = state
     }
 
+    @Published
+    var list = (0 ..< 100).map { index in (index, UIView.State.unknown) }
+}
+
+@available(iOS 13.0, *)
+struct SwiftUIDemoView: View {
+    @ObservedObject
+    var viewModel = SwiftUIDemoViewModel()
+
     var body: some View {
-        List(0 ..< 100) { index in
+        List(viewModel.list, id: \.0) { index, state in
             CellView(index: index)
                 .frame(height: 100)
+                .background(state.isImpressed ? Color.green : Color.red)
                 .detectImpressionForGroup(onCreated: { view in
-                    group.bind(view: view, index: index)
+                    viewModel.group.bind(view: view, index: index)
                 })
         }
     }
