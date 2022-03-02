a687b8fc482230b1b2d08b129cf7ea1fe72164db
Yanni Wang <wy19900729@gmail.com>
2022-03-02 20:42:56 +0800

    3

--- ImpressionKitExample/ImpressionKitExampleUITests/ImpressionKitExampleUITestsLaunchTests.swift
+++ ImpressionKitExample/ImpressionKitExampleUITests/ImpressionKitExampleUITestsLaunchTests.swift
@@ -17,16 +17,16 @@ class ImpressionKitExampleUITestsLaunchTests: XCTestCase {
         continueAfterFailure = false
     }
 
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
+//    func testLaunch() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Insert steps here to perform after app launch but before taking a screenshot,
+//        // such as logging into a test account or navigating somewhere in the app
+//
+//        let attachment = XCTAttachment(screenshot: app.screenshot())
+//        attachment.name = "Launch Screen"
+//        attachment.lifetime = .keepAlways
+//        add(attachment)
+//    }
 }
