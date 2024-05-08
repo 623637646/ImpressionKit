3c0c60e2f54f15342f43001b7ba45df611950070
Joanda Febrian <jfe@mega.co.nz>
2024-05-08 10:23:42 +0700

    Fix build error when running unit tests caused by code coverage
    This commit is added to make ImpressionKit compatible with our project and fix the build error issue when running unit tests with code coverage on in the main project.

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ImpressionKit",
+            type: .dynamic,
             targets: ["ImpressionKit"]),
     ],
     dependencies: [
